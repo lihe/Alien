ab8fd6c628617a50716f21b4aa4aec27fc99beec
lihe <lihellowd@outlook.com>
2020-12-30 08:07:13 +0800

    Initial commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    # 存储《入侵外星人》的所有设置的类
+
+    def __init__(self):
+        # 初始化游戏的设置
+
+        self.screen_widht = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
